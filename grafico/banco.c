0da96ab3716905195a954bcaec6606ee5ac8eabb
Josimar Viana Silva <josimarviana@gmail.com>
2022-05-24 18:02:26 -0300

    banco2

--- grafico/banco.c
+++ grafico/banco.c
@@ -15,5 +15,6 @@ sudo apt install mysql-server
 sudo apt-get install default-libmysqlclient-dev
 gcc  banco.c  -o banco `mysql_config --cflags --libs`
 https://zetcode.com/db/mysqlc/
+https://www.treinaweb.com.br/blog/utilizando-o-mysql-em-uma-aplicacao-c
 gcc -I/usr/include/mysql banco.c /usr/include/mysql/mysql.h -o banco `mysql_config --cflags --libs`
 */
