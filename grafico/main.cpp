dcd8d0ea1c74b41effb21dfd6f87239061898ab6
Josimar Viana Silva <josimarviana@gmail.com>
2022-05-24 17:02:43 -0300

    com qt

---
+++ grafico/main.cpp
@@ -0,0 +1,21 @@
+#include <QApplication>
+#include <QLabel>
+#include <QWidget>
+
+int main(int argc, char **argv){
+
+    QApplication app(argc, argv);
+    QLabel ola("<h1><center>Meu primeiro programa com Qt e C++");
+    ola.setWindowTitle("Título da minha janela");
+    ola.resize(600,400);
+    ola.show();
+    return app.exec();
+}
+
+/*
+sudo apt install qtcreator build-essential qt5-qmake qtbase5-dev
+qmake -project
+QT += gui widgets
+qmake
+make
+*/
