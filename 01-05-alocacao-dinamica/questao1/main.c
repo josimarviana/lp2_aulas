47c33b746cdaf364718e7cd29bcb08af9542f011
josimarviana <josimarviana@gmail.com>
2023-03-19 12:32:04 -0300

    inicio

--- 01-05-alocacao-dinamica/questao1/main.c
+++ 01-05-alocacao-dinamica/questao1/main.c
@@ -1,3 +1,11 @@
-//
-// Created by josimar on 19/03/23.
-//
+//Escreva um programa em C que leia 10 números de ponto flutuante
+//em um vetor. Em seguida, o seu programa deve calcular a média
+//dos valores armazenados no vetor e imprimir este valor.
+//Use alocação dinâmica de memória.
+
+#include "stdio.h"
+
+int main (int argc, char ** argv){
+    
+    printf ("teste");
+}
---
+++ 01-05-alocacao-dinamica/questao2/main.c
@@ -0,0 +1,11 @@
+//Escreva um programa em C que leia 10 números inteiros em um vetor. 
+//Em seguida, o seu programa deve ler um outro número inteiro x. 
+//O programa deve então encontrar dois números de posições distintas do 
+//vetor cuja multiplicação seja x e imprimi-los.
+//Caso não existam tais números, o programa deve informar isto.
+//Use alocação dinâmica de memória.
+
+//int v[] = {2, 4, 5, -10, 7, 3, 2, 1, 0, 1};
+//x = 35;
+//Resultado = 5, 7
+
