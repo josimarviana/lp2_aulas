5cfe209501006162b66ddca8c8b1eb97b1893213
josimarviana <josimarviana@gmail.com>
2023-03-28 17:57:10 -0300

    questao5 - soma das linhas

--- 01-05-alocacao-dinamica/questao5/main.c
+++ 01-05-alocacao-dinamica/questao5/main.c
@@ -11,26 +11,45 @@
 #include<time.h>
 int main (int argc, char ** argv) {
     srand(time(NULL));
-    int n=0;
+    int n=0, maximo=2;
     printf("Informe a quantidade de elementos em cada linha ou coluna: ");
     scanf("%d", &n);
 
     int *pInt = (int *) malloc(sizeof(int)*n*n);
 
-    for (int i = 0; i < n*n; i++) {
+/*    for (int i = 0; i < n*n; i++) {
         //printf("Digite o %do número do vetor 1: ", i+1);
         //scanf("%d", &pInt[i]);
-        pInt[i] = rand() % 2;
-    }
+        pInt[i] = rand() % maximo; //gera aleatório de 0 até maximo-1
+    }*/
 
+    int auxn=0, auxm=0, m=n;
+    for (int i = 0; i < n*n; i++) {
+        printf("Digite a posição [%d][%d] da matriz: ", auxn, auxm);
+        scanf("%d", &pInt[i]);
+        auxm++;
+        if(auxm>=m){
+            auxm=0;
+            auxn++;
+        }
+    }
+    int *pvetorColunas = (int *) malloc(sizeof(int)*n);
+    int *pvetorLinhas = (int *) malloc(sizeof(int)*n);
+    int *pdiagPrincipal = (int *) malloc(sizeof(int)*n);
+    int *pdiagSecundaria = (int *) malloc(sizeof(int)*n);
+    int acumula=0, soma=0, controle=0;
     for (int i = 0; i < n*n; i++) {
             printf("%d ", pInt[i]);
-            if((i+1)%n==0)
+            soma+=pInt[i];
+            if((i+1)%n==0){
+                pvetorLinhas[controle]=soma;
+                controle++;
+                soma=0;
                 printf("\n");
+            }
     }
-
-
-    //rand() % 100;
-
-
+    printf("\nExibindo soma das linhas:");
+    for (int i = 0; i < controle; i++) {
+        printf("%d ", pvetorLinhas[i]);
+    }
 }
