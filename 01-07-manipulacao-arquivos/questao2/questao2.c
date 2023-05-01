139ac8f3941b4144252e72d982b06bb2826b05b3
josimarviana <josimarviana@gmail.com>
2023-05-01 15:25:16 -0300

    arquivos

---
+++ 01-07-manipulacao-arquivos/questao2/questao2.c
@@ -0,0 +1,14 @@
+//Faça um programa para gerenciar uma agenda de contatos.
+//Para cada contato armazene o nome, o telefone e o aniversário (dia e mês), o programa deve permitir:
+//
+//- inserir contato.
+//- remover contato.
+//- pesquisar um contato pelo nome.
+//- listar todos os contatos.
+//- listar os contatos cujo nome inicia com uma dada letra.
+//- imprimir os aniversariantes do mês.
+//
+//Sempre que o programa for encerrado, os contatos devem ser armazenados em um arquivo binário.
+//Quando o programa iniciar, os contatos devem ser inicializados com os dados contidos neste arquivo binário.
+
+
---
+++ 01-07-manipulacao-arquivos/questao3/questao3.c
@@ -0,0 +1,22 @@
+//Um arquivo JSON (JavaScript Object Notation) é utilizado frequentemente para
+//transmitir dados. Basicamente trata-se de um arquivo texto no formato "chave":valor.
+//Faça um programa que, dado um arquivo JSON passado via parâmetros de linha de
+//comando, interprete-o em um registro em memória. O JSON da questão tem o
+//seguinte formato, podendo assumir valores diferentes a cada arquivo:
+//
+//```
+//{
+//"nome": "Epaminondas",
+//"sobrenome": "da Silva",
+//"vivo": 1,
+//"idade": 25,
+//"endereco": "IFTM"
+//}
+//```
+//
+//As chaves podem vir em ordem diferente a cada arquivo,
+//então você deverá utilizar o nome delas para identificar em qual
+//campo do registro os valores devem ser gravados.
+//Após converter um arquivo JSON na sua representação em registro,
+//o seu programa deverá dar a opção de alterar os dados em memória e sobrescrever o arquivo original.
+//Referência: https://en.wikipedia.org/wiki/JSON
